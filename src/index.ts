d24623423fc316e8d34c4026094972fd4a3daeed
Stephen Leitnick <sleitnick@gmail.com>
2022-08-08 18:58:01 -0400

    Util

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./core";
 export * from "./net";
 export * from "./lifecycle";
+export * from "./util";
---
+++ src/util.ts
@@ -0,0 +1,50 @@
+export namespace ProtonUtil {
+	/**
+	 * Load Mode for `ProtonUtil.loadModules()` function.
+	 */
+	export enum LoadMode {
+		/**
+		 * Only iterate over direct children instances.
+		 */
+		Children,
+
+		/**
+		 * Iterate over all descendant instances.
+		 */
+		Descendants,
+	}
+
+	/**
+	 * Load all modules within the parent of the calling script.
+	 * An optional `matchName` and `loadMode` can be provided
+	 * to help dictate the load behavior.
+	 *
+	 * ```ts
+	 * // Load all children ModuleScripts
+	 * ProtonUtil.loadModules();
+	 *
+	 * // Load all descendant ModuleScripts
+	 * ProtonUtil.loadModules(undefined, ProtonUtil.LoadMode.Descendants);
+	 *
+	 * // Load all ModuleScripts that end with '-service'
+	 * ProtonUtil.loadModules("%-service$", ProtonUtil.LoadMode.Descendants);
+	 * ```
+	 *
+	 * @param matchName Only load ModuleScripts that match the given pattern
+	 * @param loadMode LoadMode enum to indicate scanning direct children or all descendants
+	 */
+	export function loadModules(matchName?: string, loadMode: LoadMode = LoadMode.Children) {
+		const parent = script.Parent;
+		if (!parent) {
+			error("[Proton]: The calling script of `ProtonUtil.loadModules()` must have a parent", 2);
+		}
+		const instances = loadMode === LoadMode.Children ? parent.GetChildren() : parent.GetDescendants();
+		for (const instance of instances) {
+			if (instance.IsA("ModuleScript")) {
+				if (matchName === undefined || instance.Name.match(matchName)[0] !== undefined) {
+					require(instance);
+				}
+			}
+		}
+	}
+}
